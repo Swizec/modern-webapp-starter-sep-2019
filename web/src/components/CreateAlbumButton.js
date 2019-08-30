a95a1d9e8f996097f3ba12ac8afe9e31a246b1dc
Swizec Teller <swizec@swizec.com>
2019-08-30 08:25:20 +0200

    Change album button into link

--- web/src/components/CreateAlbumButton.js
+++ web/src/components/CreateAlbumButton.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { useAuth } from "react-use-auth"
-import { Button, Box } from "rebass"
+import { Button, Box, Text } from "rebass"
 import { useMutation } from "react-apollo-hooks"
 import { BarLoader } from "react-spinners"
+import { Link } from "gatsby"
 
 import { CREATE_ALBUM } from "../queries"
 
@@ -11,16 +12,24 @@ const CreateAlbum = ({ userId }) => {
     variables: { userId },
   })
 
-  console.log(data)
-
-  return (
-    <Box m={[2, 3, 4]}>
-      <Button variant="secondary" onClick={createAlbum}>
-        Create new album
-      </Button>
-      {loading ? <BarLoader height={4} width={169} /> : null}
-    </Box>
-  )
+  if (!data) {
+    return (
+      <Box m={[2, 3, 4]}>
+        <Button variant="secondary" onClick={createAlbum}>
+          Create new album
+        </Button>
+        {loading ? <BarLoader height={4} width={169} /> : null}
+      </Box>
+    )
+  } else {
+    return (
+      <Box m={[2, 3, 4]}>
+        <Link to={`/${data.createAlbum.albumId}`}>
+          <Text>Visit your album</Text>
+        </Link>
+      </Box>
+    )
+  }
 }
 
 const CreateAlbumButton = () => {
