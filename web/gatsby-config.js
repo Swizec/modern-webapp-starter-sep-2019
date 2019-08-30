9fcbc347cd132fcab72aee7b64101d0ed2250016
Swizec Teller <swizec@swizec.com>
2019-08-30 08:43:10 +0200

    Gatsby graphql api hookup

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,34 +1,46 @@
 module.exports = {
-    siteMetadata: {
-        title: `Shared photo album`,
-        description: `Create shared photo albums and vote on the best pics`,
-        author: `@swizec`,
+  siteMetadata: {
+    title: `Shared photo album`,
+    description: `Create shared photo albums and vote on the best pics`,
+    author: `@swizec`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ],
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "ALBUMAPI",
+        // This is field under which it's accessible
+        fieldName: "albumapi",
+        // Url to query from
+        url:
+          "https://hkj1n72138.execute-api.us-east-1.amazonaws.com/dev/graphql",
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
