0fba8adf300d7a023233418e3283308e95abe25d
Swizec Teller <swizec@swizec.com>
2019-09-03 01:11:54 +0200

    Cleanup server side

--- server/src/graphql.ts
+++ server/src/graphql.ts
@@ -1,61 +1,20 @@
 import { ApolloServer, gql } from "apollo-server-lambda"
 
-import { album, allAlbum, presignedUploadUrl } from "./queries"
-import { createAlbum, addImage, likeImage } from "./mutations"
+import {} from "./queries"
+import {} from "./mutations"
 
 const typeDefs = gql`
-  type Album {
-    userId: String!
-    albumId: String!
-    createdAt: String
-    images: [Image]
-  }
-
-  type Image {
-    albumId: String!
-    imageId: String!
-    imageUrl: String
-    createdAt: String
-    likes: Int
-    dislikes: Int
-  }
-
-  type PresignedUrl {
-    uploadUrl: String
-    readUrl: String
-    imageId: String
-    expiresAt: String
-  }
-
   type Query {
-    album(userId: String!, albumId: String!): Album
-    allAlbum(userId: String): [Album]
-    presignedUploadUrl(albumId: String!): PresignedUrl
   }
 
   type Mutation {
-    createAlbum(userId: String!): Album
-    addImage(albumId: String!, imageId: String!, imageUrl: String!): Image
-    likeImage(
-      albumId: String!
-      imageId: String!
-      like: Boolean
-      dislike: Boolean
-    ): Image
+    
   }
 `
 
 const resolvers = {
-  Query: {
-    allAlbum,
-    album,
-    presignedUploadUrl,
-  },
-  Mutation: {
-    createAlbum,
-    addImage,
-    likeImage,
-  },
+  Query: {},
+  Mutation: {},
 }
 
 const server = new ApolloServer({ typeDefs, resolvers })
