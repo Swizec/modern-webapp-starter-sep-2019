2b723206424df57cf72d9eef43c7dd32648056a6
Swizec Teller <swizec@swizec.com>
2019-09-02 16:14:14 +0200

    Reflow album grid

--- web/src/components/ImagesList.js
+++ web/src/components/ImagesList.js
@@ -4,7 +4,12 @@ import { Heading, Card, Image, Box } from "rebass"
 export default ({ images }) => (
   <Box mt={20} mb={40}>
     {images.map(img => (
-      <Card width={[512, 256, 384]} key={img.imageId}>
+      <Card
+        width={[128, 256]}
+        key={img.imageId}
+        m={[2, 3, 4]}
+        sx={{ display: "inline-block" }}
+      >
         <Image src={img.imageUrl} />
         <Heading fontSize={[2, 3, 4]}>{img.imageId}</Heading>
       </Card>
